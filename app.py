39e36b9e2fc5346e2b4cbf7a181cbd2bbc673a76
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-06-02 03:44:13 +0300

    Get/Post/Patch/Delete

--- app.py
+++ app.py
@@ -9,3 +9,33 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {"message": "Welcome to my first backend app"}
+
+# member
+# http://localhost:8000/member
+@app.get('/member')
+def member():
+    # use sqlalchemy to retrieve all member from the db
+    return[]
+
+# http://localhost:8000/member
+@app.post('/member')
+def member():
+    return{"message": "Member added successfully"}
+
+# http://localhost:8000/member/3
+@app.get('/member/{member_id}')
+def get_member(member_id: int):
+    print("Member id:", member_id)
+    return {}
+
+# http://localhost:8000/member/3
+@app.patch('/member/{id}')
+def get_member(id: int):
+    print(f"product of id {id} updatedd")
+    return {"message": "Member updated successfully"}
+
+# http://localhost:8000/member/3
+@app.delete('/member/{id}')
+def get_member(id: int):
+    print(f"Member of id {id} deleted")
+    return {"message": "Member deleted successfully"}
